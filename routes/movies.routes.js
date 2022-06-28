83ace28e9f4ad4eb9e050b8f3221d9886f665483
ericruga <ericruga@users.noreply.github.com>
2022-06-28 19:22:19 +0200

    Half Iteration 6 done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -10,5 +10,23 @@ router.get("/create", (req, res, next) => {
   res.render("movies/new-movie.hbs")
 });
 
+router.post("/create", (req, res, next) => {
+  const { name } = req.body;
+  Celebrity.findOne({ name })
+    .then((celebritiesDocFromDB) => {
+      if (!celebritiesDocFromDB) {
+        // prettier-ignore
+        Celebrity.create({ name, genre, plot })
+        .then(() => res.redirect('/movies'));
+      } else {
+        res.render("movies/new-movie.hbs", { message: "It seems you are already registered. ☀️" });
+        return;
+      }
+    })
+    .catch((err) => {
+      console.log(`Error while creating a new user: ${err}`);
+      next(err);
+    });
+});
 
 module.exports = router;
